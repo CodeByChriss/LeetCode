44bccfeb892027f83619fe7711e89188c7e09db2
Christian Salguero <105109766+CodeByChriss@users.noreply.github.com>
2025-02-23 15:05:37 +0100

    Time: 7 ms (16.90%) | Memory: 43.3 MB (5.94%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+        String devolver = "";
+        int mayor= word1.length() > word2.length() ? word1.length() : word2.length();
+        int f=0,k=0;
+
+        for(int i = 0; i<mayor*2;i++){
+            if(i == 0 || i % 2 == 0){
+                if(f< word1.length()){
+                    devolver += word1.charAt(f);
+                    f++;
+                }
+            }else{
+                if(k < word2.length()){
+                    devolver += word2.charAt(k);
+                    k++;
+                }
+            }
+        }
+
+        return devolver;
+    }
+}
