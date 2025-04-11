6dcc358d2df648f7d1601d136918e8defda2e73c
Christian Salguero <105109766+CodeByChriss@users.noreply.github.com>
2025-04-11 11:19:50 +0200

    Time: 145 ms (6.49%) | Memory: 44.6 MB (52.09%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.java
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -1,10 +1,10 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        int cnt = 0;
+        int cnt=0, p1=0, p2=0, mid=0;
         for(;low<=high;low++){
             if(String.valueOf(low).length() % 2 == 0){
-                int mid = String.valueOf(low).length() / 2;
-                int p1=0, p2=0;
+                mid = String.valueOf(low).length() / 2;
+                p1=p2=0;
                 for(int i = 0; i<mid; i++){
                     p1 += Integer.parseInt(String.valueOf(String.valueOf(low).charAt(i)));
                     p2 += Integer.parseInt(String.valueOf(String.valueOf(low).charAt(mid+i)));
