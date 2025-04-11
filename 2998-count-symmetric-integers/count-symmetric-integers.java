2d66e5bf48b21bab31fd11f4ae217de585e4e158
Christian Salguero <105109766+CodeByChriss@users.noreply.github.com>
2025-04-11 11:17:54 +0200

    Time: 146 ms (6.49%) | Memory: 45.1 MB (13.42%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.java
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -5,22 +5,14 @@ class Solution {
             if(String.valueOf(low).length() % 2 == 0){
                 int mid = String.valueOf(low).length() / 2;
                 int p1=0, p2=0;
-                p1 = getNumbers(String.valueOf(low), 0, mid);
-                p2 = getNumbers(String.valueOf(low), mid, mid);
-                if(p1 == p2){
+                for(int i = 0; i<mid; i++){
+                    p1 += Integer.parseInt(String.valueOf(String.valueOf(low).charAt(i)));
+                    p2 += Integer.parseInt(String.valueOf(String.valueOf(low).charAt(mid+i)));
+                }
+                if(p1 == p2)
                     cnt++;
-                    System.out.println("yes -> "+p1+" = "+p2);
-                }else{System.out.println("no -> "+p1+" = "+p2);}
             }
         }
         return cnt;
-    }
-
-    public static int getNumbers(String low, int add, int mid){
-        int p=0;
-        for(int i = 0; i<mid; i++){
-            p += Integer.parseInt(String.valueOf(low.charAt(add+i)));
-        }
-        return p;
     }
 }
